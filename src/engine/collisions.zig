d7e425d9f1678fcde53530a9b0de6d1ce967dcdd
Darlan Pacheco <darlanpacheco@proton.me>
2026-07-31 02:42:33 +0000

    engine 0.0.8

--- src/engine/collisions.zig
+++ src/engine/collisions.zig
@@ -4,32 +4,33 @@ const engine = @import("engine");
 
 pub fn next_step_2d(
     id: usize,
-    size: [2]i32,
-    position: [2]i32,
+    size: [2]f32,
+    position: [2]f32,
     velocity: [2]f32,
-    hitboxes_size: []const [2]i32,
-    hitboxes_position: []const [2]i32,
+    hitboxes_size: []const [2]f32,
+    hitboxes_position: []const [2]f32,
 ) bool {
-    const position_next = [2]i32{
-        position[0] + @as(i32, @intFromFloat(@round(velocity[0]))),
-        position[1] + @as(i32, @intFromFloat(@round(velocity[1]))),
+    const position_next = [2]f32{
+        position[0] + velocity[0],
+        position[1] + velocity[1],
     };
 
-    for (hitboxes_position, hitboxes_size, 0..) |h_pos, h_size, index| {
+    for (hitboxes_position, hitboxes_size, 0..) |hitbox_position, hitbox_size, index| {
         if (index == id) {
             continue;
         }
 
-        if (h_size[0] == 0 or h_size[1] == 0) {
+        if (hitbox_size[0] <= 0 or hitbox_size[1] <= 0) {
             continue;
         }
 
-        const collision = [2]bool{
-            position_next[0] < h_pos[0] + h_size[0] and position_next[0] + size[0] > h_pos[0],
-            position_next[1] < h_pos[1] + h_size[1] and position_next[1] + size[1] > h_pos[1],
-        };
+        const overlap =
+            position_next[0] < hitbox_position[0] + hitbox_size[0] and
+            position_next[0] + size[0] > hitbox_position[0] and
+            position_next[1] < hitbox_position[1] + hitbox_size[1] and
+            position_next[1] + size[1] > hitbox_position[1];
 
-        if (collision[0] and collision[1]) {
+        if (overlap) {
             return true;
         }
     }
